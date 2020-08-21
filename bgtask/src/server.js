d3c4f6b69bf9231794239b88cb632457c5727f5a
Leonardo <leo.csz@hotmail.com>
2020-08-21 16:47:24 -0300

    importing bullboard and queue

--- bgtask/src/server.js
+++ bgtask/src/server.js
@@ -4,14 +4,20 @@ import express from 'express';
 import userController from './app/controllers/userController';
 
 import passwordGenerator from 'password-generator';
+import BullBoard from 'bull-board';
+import queue from './app/lib/queue'
 
 
 
 const app = express();
+BullBoard.setQueues(queue.queues.map(queue => queue.bull))
+
 
 app.use(express.json());
 
 app.post('./users', userController.store);
+
+app.use('/admin/queues', BullBoard.UI)
 
 app.listen(process.env.PORT, () =>{
     console.log(`Server running on the ${process.env.PORT}`)
