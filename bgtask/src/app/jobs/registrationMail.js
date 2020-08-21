aaeb75922d021968c02ff45489822cb461065226
Leonardo <leo.csz@hotmail.com>
2020-08-21 16:47:09 -0300

    delay registration, changing user.name

--- bgtask/src/app/jobs/registrationMail.js
+++ bgtask/src/app/jobs/registrationMail.js
@@ -3,16 +3,17 @@ import mail from '../lib/mail'
 export default{
     key: 'registrationMail',
     options: {
-        
+        delay: 5000,
+        priority:3
     },
     async handle({ data }){
         const { user } = data;
 
         await mail.sendMail({
             from: 'DIO <contato@dio.com.br',
-            to: '${name} <${email}>',
+            to: '${user.name} <${user.email}>',
             subject: 'Cadastro de usuario',
-            html: ' Olá, ${name}, bem-vindo a Proffy'
+            html: ' Olá, ${user.name}, bem-vindo a Proffy'
         })
     }
 }
